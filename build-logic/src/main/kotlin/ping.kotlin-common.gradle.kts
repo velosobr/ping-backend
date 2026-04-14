02a3b1cfe10b45e6cc30338d96937944fed05892
velosobr <linoc.veloso@gmail.com>
2026-04-13 23:15:22 -0300

    Kotlin Common Convention Plugin

--- build-logic/src/main/kotlin/ping.kotlin-common.gradle.kts
+++ build-logic/src/main/kotlin/ping.kotlin-common.gradle.kts
@@ -1,6 +1,35 @@
-import org.gradle.kotlin.dsl.kotlin
+import org.gradle.api.plugins.JavaPluginExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import kotlin.collections.addAll
 
 plugins {
     kotlin("jvm")
     kotlin("plugin.spring")
+    id("io.spring.dependency-management")
+}
+
+repositories {
+    mavenCentral()
+}
+
+configure<KotlinJvmProjectExtension> {
+    jvmToolchain(21)
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:${libraries.findVersion("spring-boot").get()}")
+    }
+}
+tasks.withType<KotlinCompile> {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+        jvmTarget = JvmTarget.JVM_21
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
